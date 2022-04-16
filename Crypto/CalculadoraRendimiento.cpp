eb7d165d6848a3bb7bc0f1b7d574ae9121a5c056
FabiLatronico <fabilatronico@frba.utn.edu.ar>
2022-04-15 23:46:51 -0300

    Se agregan mas cambios

--- Crypto/CalculadoraRendimiento.cpp
+++ Crypto/CalculadoraRendimiento.cpp
@@ -1,5 +1,6 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include <stdlib.h>
 #include<cstdio>
 #include<iostream>
 #include<conio.h>
@@ -11,20 +12,60 @@ using namespace std;
 void RentabilidadConAltcoin();
 void RentabilidadConUsdt();
 void menu();
+void clrscr();
 void RentabilidadConTenenciaEnAltcoin();
 //void Grafico();
 
 int main(){
 	
 menu();
-
-system("pause");
 return 0;
+}
+
+void clrscr()
+{
+    system("@cls||clear");
+}
+
+void menu(){
+	clrscr();
+	int opcion;
+	
+	do
+    {
+		printf("Elija operacion a realizar \n\n");
+		printf(" 1. Si ya tenes Altcoin pero no te acordas a que precio compraste \n	y te acordas cuantos usdt invertiste inicialmente \n\n");
+		printf(" 2. Si tenes USDT y queres calcular rentabilidad comprando y vendiendo x Altcoin \n	para un precio de compra y un precio de venta puntuales \n\n");
+		printf(" 3. Si tenes USDT y queres proyectar los porcentajes de ganancia y perdida \n\n");
+		printf(" 4. Salir \n\n");
+		printf( "Introduzca opci%cn (1-4): \n", 162 );
+		printf("Opcion elegida -> ");
+		scanf("%d",&opcion);
+		printf("\n\n\n");
+		
+		clrscr();
+		switch(opcion) //donde opción es la variable a comparar
+			{
+			    case 1: RentabilidadConTenenciaEnAltcoin();
+			    		clrscr();
+			    break;
+			    case 2: RentabilidadConAltcoin();
+			    		clrscr();
+			    break;
+			    case 3: RentabilidadConUsdt();
+			    		clrscr();
+			    break;
+			    
+			}
+	}while(opcion!=4);
+
+
+	
 }
 
 void RentabilidadConAltcoin()
 {
-	
+
 	float precioCompra;
 	float precioVenta;
 	float usdt;
@@ -43,56 +84,25 @@ void RentabilidadConAltcoin()
 	printf("Ingrese precio de venta de %s \n",altcoin);
 	scanf("%f",&precioVenta);
 	
-	printf("\n\n\n\n");
+	clrscr();
 	float CantidadAltcoin = usdt / precioCompra;
 	float balance=CantidadAltcoin * precioVenta;
 	float porcentaje = (balance /usdt *100 ) -100;
 	
 	printf("Tenencia %.2f %s | %.2f USDT | %.2f ARS \n",CantidadAltcoin,altcoin,usdt,usdt*190);
 		
-	printf("%s  %.2f USDT | %.2f %%\n",precioCompra<precioVenta ? "Ganancia" : "Perdida",balance-usdt,porcentaje);	
+	printf("%s  %.2f USDT | %.2f %%\n\n",precioCompra<precioVenta ? "Ganancia" : "Perdida",balance-usdt,porcentaje);	
 
 	
+	system("pause");
+
 }
 
 
-void menu(){
-	int opcion;
-	
-	do
-    {
-		printf("Elija operacion a realizar \n\n");
-		printf(" 1. Si ya tenes Altcoin pero no te acordas a que precio compraste \n	y te acordas cuantos usdt invertiste inicialmente \n\n");
-		printf(" 2. Si tenes USDT y queres calcular rentabilidad comprando y vendiendo x Altcoin \n	para un precio de compra y un precio de venta puntuales \n\n");
-		printf(" 3. Si tenes USDT y queres proyectar los porcentajes de ganancia y perdida \n\n");
-		printf(" 4. Salir \n\n");
-		printf( "Introduzca opci%cn (1-4): \n", 162 );
-		printf("Opcion elegida -> ");
-		scanf("%d",&opcion);
-		printf("\n\n\n");
-	
-		switch(opcion) //donde opción es la variable a comparar
-			{
-			    case 1: RentabilidadConTenenciaEnAltcoin();
-			    		printf("\n\n\n");
-			    break;
-			    case 2: RentabilidadConAltcoin();
-			    		printf("\n\n\n");
-			    break;
-			    case 3: RentabilidadConUsdt();
-			    		printf("\n\n\n");
-			    break;
-			  //  default: 
-				//	printf("Opcion equivocada \n");
-				//	printf("Otaku olor a pata :v\n No te olvides de darle de comer a la mama \n\n\n\n");
-			}
-	}while(opcion!=4);
-	printf("-------------------------------------------------------------------------------------------------------------------- \n");
-	printf("\n\n");
-}
 
 void RentabilidadConTenenciaEnAltcoin()
 {
+	
 	float precioActual;
 	float tenencia;
 	float deuda;
@@ -115,32 +125,31 @@ void RentabilidadConTenenciaEnAltcoin()
 	printf("Ingrese deuda tomada\n");
  	scanf("%f",&deuda);
  	
- 	//clrscr();
-		
+
+	clrscr();	
 	bool Condicion = tenencia*precioActual>capital_incial+deuda;
 	float Equilibrio = (capital_incial+deuda)/tenencia;
 	float balance = (tenencia*precioActual)-(capital_incial+deuda);
 	float porcentaje = (((tenencia * precioActual) - deuda) / capital_incial * 100)-100;
-	printf("\n\n\n\n");
+
 	
 	
 	printf("Tenencia %.2f %s | %.2f USDT | %.2f ARS \n",tenencia,altcoin,tenencia*precioActual,tenencia*precioActual*190);
 		
 	printf("%s  %.2f USDT | %.2f %% \n",Condicion ? "Ganancia" : "Perdida",balance,porcentaje);
 	if(!Condicion) {
-		printf("\n\nPara estar en tablas \n",12.0);
-		printf("Necesito que el precio suba a -> %.2f \n",Equilibrio);
+		printf("Para estar en tablas \n",12.0);
+		printf("Necesito que el precio suba a -> %.2f \n\n",Equilibrio);
 	}
-		
-		
 	
-
-//	printf("Balance %.2f USDT | %.2f ARS\n",tenencia*precioActual,tenencia*precioActual*190);
+	
+	system("pause");
 	
 	
 }
 void RentabilidadConUsdt()
 {
+	
 	float precio;
 	float usdt;
 	
@@ -154,12 +163,11 @@ void RentabilidadConUsdt()
 	
 	printf("Cantidad de la altcoin: %f \n",cantidad);
 	
-	//Grafico();
-	
+	clrscr();
 	int i=0;	
 	for(i=1;i<=50;i++){
 	
-	//float porcentaje = 1+(i100);
+
 		float porcentaje =(i*0.01);
 		float formula = 1+ porcentaje; 
 		
@@ -168,6 +176,8 @@ void RentabilidadConUsdt()
 		}
 		
 	}
+	printf("\n\n");
+	system("pause");
 }
 
 
