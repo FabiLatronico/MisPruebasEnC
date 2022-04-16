8b315b4b7b22ec0de8fc399946a0231137ba1d23
FabiLatronico <fabilatronico@frba.utn.edu.ar>
2022-04-15 23:21:22 -0300

    Se agregan nuevas funcionalidades

--- Crypto/CalculadoraRendimiento.cpp
+++ Crypto/CalculadoraRendimiento.cpp
@@ -10,14 +10,14 @@ using namespace std;
 //Prototipos de Funciones y Procedimientos
 void RentabilidadConAltcoin();
 void RentabilidadConUsdt();
-void Grafico();
+void menu();
+void RentabilidadConTenenciaEnAltcoin();
+//void Grafico();
 
 int main(){
-	RentabilidadConAltcoin();
-//	RentabilidadConUsdt();
-	printf("-------------------------------------------------------------------------------------------------------------------- \n");
-	printf("\n\n");
 	
+menu();
+
 system("pause");
 return 0;
 }
@@ -46,19 +46,99 @@ void RentabilidadConAltcoin()
 	printf("\n\n\n\n");
 	float CantidadAltcoin = usdt / precioCompra;
 	float balance=CantidadAltcoin * precioVenta;
+	float porcentaje = (balance /usdt *100 ) -100;
 	
 	printf("Tenencia %.2f %s | %.2f USDT | %.2f ARS \n",CantidadAltcoin,altcoin,usdt,usdt*190);
-	
-	float porcentaje = (balance /usdt *100 ) -100; 
 		
 	printf("%s  %.2f USDT | %.2f %%\n",precioCompra<precioVenta ? "Ganancia" : "Perdida",balance-usdt,porcentaje);	
-
-	printf("Balance %.2f USDT | %.2f ARS\n",balance,balance*190);
 
 	
 }
 
 
+void menu(){
+	int opcion;
+	
+	do
+    {
+		printf("Elija operacion a realizar \n\n");
+		printf(" 1. Si ya tenes Altcoin pero no te acordas a que precio compraste \n	y te acordas cuantos usdt invertiste inicialmente \n\n");
+		printf(" 2. Si tenes USDT y queres calcular rentabilidad comprando y vendiendo x Altcoin \n	para un precio de compra y un precio de venta puntuales \n\n");
+		printf(" 3. Si tenes USDT y queres proyectar los porcentajes de ganancia y perdida \n\n");
+		printf(" 4. Salir \n\n");
+		printf( "Introduzca opci%cn (1-4): \n", 162 );
+		printf("Opcion elegida -> ");
+		scanf("%d",&opcion);
+		printf("\n\n\n");
+	
+		switch(opcion) //donde opción es la variable a comparar
+			{
+			    case 1: RentabilidadConTenenciaEnAltcoin();
+			    		printf("\n\n\n");
+			    break;
+			    case 2: RentabilidadConAltcoin();
+			    		printf("\n\n\n");
+			    break;
+			    case 3: RentabilidadConUsdt();
+			    		printf("\n\n\n");
+			    break;
+			  //  default: 
+				//	printf("Opcion equivocada \n");
+				//	printf("Otaku olor a pata :v\n No te olvides de darle de comer a la mama \n\n\n\n");
+			}
+	}while(opcion!=4);
+	printf("-------------------------------------------------------------------------------------------------------------------- \n");
+	printf("\n\n");
+}
+
+void RentabilidadConTenenciaEnAltcoin()
+{
+	float precioActual;
+	float tenencia;
+	float deuda;
+	float capital_incial;
+	char altcoin[20];
+	
+
+	printf("Ingrese capital inicial en tether\n");
+	scanf("%f",&capital_incial);
+	
+	printf("Ingrese nombre de Altcoin \n");
+	scanf("%s",&altcoin);
+	
+	printf("Ingrese tenencia de %s \n",altcoin);
+	scanf("%f",&tenencia);
+
+	printf("Ingrese precio actual de %s \n",altcoin);
+	scanf("%f",&precioActual);
+	
+	printf("Ingrese deuda tomada\n");
+ 	scanf("%f",&deuda);
+ 	
+ 	//clrscr();
+		
+	bool Condicion = tenencia*precioActual>capital_incial+deuda;
+	float Equilibrio = (capital_incial+deuda)/tenencia;
+	float balance = (tenencia*precioActual)-(capital_incial+deuda);
+	float porcentaje = (((tenencia * precioActual) - deuda) / capital_incial * 100)-100;
+	printf("\n\n\n\n");
+	
+	
+	printf("Tenencia %.2f %s | %.2f USDT | %.2f ARS \n",tenencia,altcoin,tenencia*precioActual,tenencia*precioActual*190);
+		
+	printf("%s  %.2f USDT | %.2f %% \n",Condicion ? "Ganancia" : "Perdida",balance,porcentaje);
+	if(!Condicion) {
+		printf("\n\nPara estar en tablas \n",12.0);
+		printf("Necesito que el precio suba a -> %.2f \n",Equilibrio);
+	}
+		
+		
+	
+
+//	printf("Balance %.2f USDT | %.2f ARS\n",tenencia*precioActual,tenencia*precioActual*190);
+	
+	
+}
 void RentabilidadConUsdt()
 {
 	float precio;
@@ -74,16 +154,18 @@ void RentabilidadConUsdt()
 	
 	printf("Cantidad de la altcoin: %f \n",cantidad);
 	
-	Grafico();
+	//Grafico();
 	
 	int i=0;	
-	for(i=1;i<=1000;i++){
+	for(i=1;i<=50;i++){
 	
 	//float porcentaje = 1+(i100);
 		float porcentaje =(i*0.01);
 		float formula = 1+ porcentaje; 
 		
-		printf("	+%d		%.2f	%.2f 	||	-%d		%.2f	%.2f   \n",i,precio*formula,(cantidad*precio*formula),i,(precio-(precio*porcentaje)),(cantidad*(precio-(precio*porcentaje))));
+		if(i % 3 == 0){
+			printf("	+%d %%		%.2f	%.2f 	||	-%d %%		%.2f	%.2f   \n",i,precio*formula,(cantidad*precio*formula),i,(precio-(precio*porcentaje)),(cantidad*(precio-(precio*porcentaje))));
+		}
 		
 	}
 }
